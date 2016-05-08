6c1e03fc3de94c6f15f9eb61398ded2e58b166d1
Barnabas Bulpett <bbulpett@charter.net>
2016-05-08 16:33:56 -0400

    Added actions to display tab content

--- app/components/tab-item.js
+++ app/components/tab-item.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	actions: {
+		clicked: function(tabItem) {
+			console.log(tabItem.title + " was selected");
+			this.sendAction("setSelectedTabItemAction", tabItem);
+		}
+	}
 });
